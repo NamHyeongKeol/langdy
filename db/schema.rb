a670b832409cde51770f817b089e6645565f659e
NamHyeongKeol <grey_pastor@naver.com>
2016-03-31 13:12:49 +0900

    rake migration

--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 20160320171036) do
     t.datetime "updated_at",                   null: false
   end
 
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
-  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
-
   create_table "feedbacks", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "text"
@@ -47,17 +37,6 @@ ActiveRecord::Schema.define(version: 20160320171036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "teacher_comments", force: :cascade do |t|
     t.integer  "commentor_id"
