9cc71a86b82ba4aaeae0db63dbd02f6687718326
NamHyeongKeol <grey_pastor@naver.com>
2016-04-06 17:19:19 +0900

    revise find_teacher detail, convert rating type integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405190452) do
+ActiveRecord::Schema.define(version: 20160406070057) do
 
   create_table "available_times", force: :cascade do |t|
     t.integer  "user_id"
@@ -77,7 +77,8 @@ ActiveRecord::Schema.define(version: 20160405190452) do
     t.string   "gender"
     t.integer  "lecture_price"
     t.integer  "lecture_number"
-    t.integer  "rating"
+    t.float    "rating"
+    t.integer  "student_number"
   end
 
   create_table "users", force: :cascade do |t|
