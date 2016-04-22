2013c99065f39253a9695ed04fc28c18c8eb8461
NamHyeongKeol <grey_pastor@naver.com>
2016-04-22 11:28:13 +0900

    가입할때 / 개인프로필 수정할때 언어 3개로 만듦

--- db/migrate/20160412143909_add_col_to_users.rb
+++ db/migrate/20160412143909_add_col_to_users.rb
@@ -1,4 +1,7 @@
 class AddColToUsers < ActiveRecord::Migration
+  # lang : 'English', 'Korean', 'Chinese'
+  # ranks of lang to learn : '선택해주세요', '초급', '중하급', '중상급', '고급'
+  # ranks of lang to teach : '선택해주세요', '중상급', '고급', '모국어'
   def change
     add_column :users, :lang_to_learn_1, :string
     add_column :users, :lang_to_learn_2, :string
