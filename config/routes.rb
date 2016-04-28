832f314602873cbcb4c340f2a0adf48e465af755
Jongmin Yoo <igniter.yoo@gmail.com>
2016-04-29 02:44:15 +0900

    fix more links for locale

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
 
-	root 'home#index'
+	root 'home#index', as: 'home'
 
 	get 'dashboard' => 'home#dashboard', as: 'dashboard'
 	get 'admin' => 'admin#index', as: 'admin'
