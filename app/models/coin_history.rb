a9bc5d6ee1f6c5efc9826431fde6cab047cc13f7
NamHyeongKeol <grey_pastor@naver.com>
2016-05-23 03:33:59 +0900

    코인관련 어드민 뷰파일 만듦, 아직 코인구매신청 처리기능 못함

--- app/models/coin_history.rb
+++ app/models/coin_history.rb
@@ -1,4 +1,27 @@
 class CoinHistory < ActiveRecord::Base
   belongs_to :user
   has_one :cash_transaction
+  has_one :exchange
+  has_one :buycoin
+
+  def type
+    if self.is_buycoin
+      return "코인 구매"
+    elsif self.is_exchange
+      return "코인 환전"
+    else
+      return "강의료 전송"
+    end
+  end
+
+  def is_complete
+    case self.type
+    when "코인 구매"
+      return self.buycoin_complete
+    when "코인 환전"
+      return self.exchange_complete
+    when "강의료 전송"
+      return self.transaction_complete
+    end
+  end
 end
